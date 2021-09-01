b65ec5872fdcd635beacfb0c318d0f07339f6161
Jinit Jain <jinit.jain@learner.manipal.edu>
2021-09-01 17:52:19 +0530

    opencv completed

--- Task-2/Opencv/15/15.py
+++ Task-2/Opencv/15/15.py
@@ -1,21 +1,20 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('D:/opencv-python-foreground.mp4')
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
-while(1):
+while True:
     ret, frame = cap.read()
 
     fgmask = fgbg.apply(frame)
 
-    cv2.imshow('fgmask', frame)
-    cv2.imshow('frame', fgmask)
+    cv2.imshow('original', frame)
+
 
     if cv2.waitKey(1) & 0xFF == ord('q'):# when the key q is pressed it exits
         break
 
 
 cap.release()
-cv2.waitKey()
 cv2.destroyAllWindows()
